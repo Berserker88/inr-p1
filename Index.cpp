d01a0c20c19f22d07a44e7b4f576172613fc21fa
chiller <simon-schiller@gmx.de>
2014-03-26 12:37:09 +0100

    that class represents the index of an dictentry with the token, number of docs and total frequency

---
+++ Index.cpp
@@ -0,0 +1,77 @@
+
+
+
+
+#include "Index.h"
+#include <sstream>
+
+
+using namespace std;
+
+
+Index::Index(string token, int freq) {
+	_token = token;
+	_ndocs = 1;
+	_tfreq = freq;
+}
+
+
+void Index::addDoc(int freq) {
+	_ndocs++;
+	_tfreq += freq;
+}
+
+
+void Index::remDoc() {
+	
+}
+
+
+void Index::addTotFreq(int freq) {
+	_tfreq += freq;
+}
+
+
+void Index::addNumDocs(int ndocs) {
+	_ndocs += ndocs;
+}
+
+
+string Index::getToken() const {
+	return _token;
+}
+
+
+int Index::getNumDocs() const {
+	return _ndocs;
+}
+
+
+int Index::getTotFreq() const {
+	return _tfreq;
+}
+
+
+bool Index::operator==(const Index& index) const {
+	return _token == index.getToken();
+}
+
+bool Index::operator<(const Index& index) const {
+	return _token < index.getToken();
+}
+
+bool Index::operator>(const Index& index) const {
+	return _token > index.getToken();
+}
+
+string Index::toString() const {
+	ostringstream os;
+	
+	os << _token << "; " << _ndocs << "; " << _tfreq;
+	
+	return os.str();
+}
+
+
+
+
