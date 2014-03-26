c3863bc8f81d05bcf6e9aa7892f04fb1928fd005
chiller <simon-schiller@gmx.de>
2014-03-26 12:35:54 +0100

    that class represents the dictionary

---
+++ IndexDict.cpp
@@ -0,0 +1,141 @@
+
+
+
+
+#include "IndexDict.h"
+
+#include <iostream>
+#include <sstream>
+
+
+using namespace std;
+
+
+
+void IndexDict::makeIndexFromList(list<Document *> doclist) {
+	
+	list<Document *>::iterator iter;
+	for(iter = doclist.begin(); iter != doclist.end(); iter++)
+		makeIndexFromDoc(*iter);
+}
+
+
+
+void IndexDict::makeIndexFromDoc(Document *doc) {
+	
+	list<string> tokens;// = tokenize(doc->getFilename());
+	
+	tokens.push_back("bla");
+	tokens.push_back("laber");
+	tokens.push_back("simon");
+	tokens.push_back("hans");
+	tokens.push_back("kuh");
+	tokens.push_back("laber");
+	tokens.push_back("bla");
+	tokens.push_back("laber");
+	tokens.push_back("gas");
+	
+	tokens.sort();
+	
+	/*
+	list<string>::iterator iter = tokens.begin();
+	for(iter; iter != tokens.end(); iter++)
+		cout << *iter << endl;
+	*/
+	
+	map<string, int> freq;
+	
+	
+	list<string>::iterator outer_iter, inner_iter;
+	string token;
+	int cnt;
+	outer_iter = tokens.begin();
+	while(outer_iter != tokens.end())
+	{
+		cnt = 1;
+		token = *outer_iter;
+
+		inner_iter = outer_iter;
+		inner_iter++;
+		while(inner_iter != tokens.end() && *inner_iter == token)
+		{
+			cnt++;
+			inner_iter++;
+		}
+		outer_iter = inner_iter;
+		
+		Posting p(doc, cnt);
+		addToDict(token, p);
+	}
+}
+
+
+void IndexDict::addToDict(string token, Posting post) {
+	Index idx(token, post.getFreq());
+	
+	map<Index, list<Posting> >::iterator iter = _dict.find(idx);
+	if(iter == _dict.end())
+	{
+		list<Posting> postings;
+		postings.push_back(post);
+		_dict.insert(pair<Index, list<Posting> >(idx, postings));
+	}
+	else
+	{
+		idx.addTotFreq(iter->first.getTotFreq());
+		idx.addNumDocs(iter->first.getNumDocs());
+		iter->second.push_back(post);
+		
+		pair<Index, list<Posting> > p(idx, iter->second);
+		_dict.erase(iter);
+		_dict.insert(p);
+	}
+}
+
+
+
+void IndexDict::clear() {
+	_dict.clear();
+}
+
+
+
+list<Posting> IndexDict::get(string token) {
+	Index idx(token);
+	
+	
+}
+
+
+
+
+string IndexDict::toString() const {
+	ostringstream os;
+	
+	map<Index, list<Posting> >::const_iterator iter;
+	
+	for(iter = _dict.begin(); iter != _dict.end(); iter++)
+	{
+		os << "{ " << iter->first.toString() << " } : { ";
+		list<Posting>::const_iterator it;
+		os << "{ ";
+		for(it = iter->second.begin(); it != iter->second.end(); it++)
+			os << it->toString() << ";";
+		os << " } " << endl;
+	}
+	
+	return os.str();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
