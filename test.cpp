5427a14f96b3902c3413e4f0ee9ded0f213640b0
chiller <simon-schiller@gmx.de>
2014-04-01 13:58:09 +0200

    added new class to debug makefile

--- test.cpp
+++ test.cpp
@@ -9,14 +9,17 @@
 #include "tinydir.h"
 #include "IndexDict.h"
 #include "Posting.h"
+#include "QueryParser.h"
+#include <sys/time.h>
 
 
 
 using namespace std;
 
 
-void printRes(list<Posting> pl) {
-	cout << "Result:" << endl << "num of docs = " << pl.size() << endl;
+void printRes(list<Posting> pl, float time) {
+	cout.precision(3);
+	cout << pl.size() << " results  -  took " << time << " ms" << endl;
 	list<Posting>::iterator p_iter;
 	for(p_iter = pl.begin(); p_iter != pl.end(); p_iter++)
 		cout << p_iter->toString() << endl;
@@ -25,20 +28,30 @@ void printRes(list<Posting> pl) {
 
 
 
+double second()
+{
+    struct timeval tm;
+    double t ;
+    gettimeofday(&tm,NULL);
+    t = (double) (tm.tv_sec) + ((double) (tm.tv_usec))/1.0e6;
+    return t;
+}
+
+
 int main( int argc, char** argv )
 {
 
 	int tnum = 1;
-	
+	srand(time(NULL));
 	// create documents from directory path
 	list<Document *> docs = Document::getDocsFromDir("CorpusUTF8");
 	
 	// print documents loaded
 	cout << "num of docs = " << docs.size() << endl;
 	list<Document *>::iterator iter;
-	for(iter = docs.begin(); iter != docs.end(); iter++)
-		cout << (*iter)->toString() << endl;
-	cout << endl << docs.front()->toString() << endl;
+	//for(iter = docs.begin(); iter != docs.end(); iter++)
+	//	cout << (*iter)->toString() << endl;
+	//cout << endl << docs.front()->toString() << endl;
 
 	// create index dictionary
 	IndexDict dict;
@@ -48,60 +61,64 @@ int main( int argc, char** argv )
 	dict.makeIndexFromList(docs);
 	//cout << "dict = " << dict.toString() << endl;
 	
+	QueryParser qp(&dict);
+	string query;
 	
-	/*
-	// query ONE string
-	string token = "oder";
-	list<Posting> plist = dict.get(token);
-	if(!plist.empty())
+	
+	double start, stop;
+	while(1)
 	{
-		// print result
-		cout << "Success: " << token << " found!" << endl;
-		list<Posting>::iterator iter;
-		for(iter = plist.begin(); iter != plist.end(); iter++)
-			cout << iter->toString() << endl;
-		cout << "num of matches = " << plist.size() << endl;
-		cout << "total frequency of '" << token << "' = " << dict.getTotFreq(token) << endl;
+		cout << "\n\nenter your query: AND=(,) OR=()() q=exit" << endl;
+		getline(cin, query);
+		if(query == "q")
+			break;
+		
+		list<Posting> result;
+		start = second();
+		result = qp.parseAndExecute(query);
+		stop = second();
+		cout << (float) start << endl;
+		cout << (float) stop << endl;
+		printRes(result, (stop-start) * 1000);
 	}
-	else
-	{
-		cout << "FAIL: " << token << " not found!" << endl;
-	}
-	*/
+	
+	
+
 	
 	
 	list<string> l1, l2, l3;
+	
+	
 	l1.push_back("hexe");
-	l1.push_back("NOT könig");
-	l2.push_back("NOT aladdin");
-	l2.push_back("NOT prinzessin");
+	l1.push_back("könig");
+	//l2.push_back("NOT aladdin");
+	//l2.push_back("NOT prinzessin");
 	//l3.push_back("hänsel");
 	//l3.push_back("gretel");
-	l3.push_back("wald");
+	//l3.push_back("wald");
 	
-
+	
 	list<Posting> res1 = dict.intersect(l1);
 	list<Posting> res2 = dict.intersect(l2);
-	list<Posting> res3 = dict.intersect(l3);
+	//list<Posting> res3 = dict.intersect(l3);
 	
 	list<Posting> res12 = dict.unionLists(res1, res2);
-	list<Posting> res123 = dict.unionLists(res12, res3);
+	//list<Posting> res123 = dict.unionLists(res12, res3);
+	
+	
+	cout << "query: hexe AND könig" << endl;
+	printRes(res1, 0.0);
 	
 	/*
-	string query;
-	list<string>::iterator s_iter;
-	for(s_iter = l.begin(); s_iter != l.end(); s_iter++)
-	{
-		query += *s_iter + ",";
-	}
-	cout << "query result for '" << query << "':" << endl;
+	cout << "query: NOT könig" << endl;
+	printRes(res2);
+	//printRes(res3);
+	
+	cout << "query: NOT könig OR hexe" << endl;
+	printRes(res12);
+	//printRes(res123);
 	*/
 	
-	printRes(res1);
-	printRes(res2);
-	printRes(res3);
-	printRes(res12);
-	printRes(res123);
 	
 	
 
