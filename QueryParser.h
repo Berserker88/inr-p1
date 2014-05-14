b8f0253e1a742eff82cb2de1e0877f5bd1df467e
chiller <simon-schiller@gmx.de>
2014-05-14 13:52:27 +0200

    added parser for fuzzy logic

--- QueryParser.h
+++ QueryParser.h
@@ -16,11 +16,13 @@ class QueryParser {
 		list<Posting> andQuery(string query);
 		string trim(string token);
 		void printResult(list<Posting> pl);
+		list<Posting> fuzzyAndQuery(string query);
 		
 	public:
 		QueryParser(IndexDict *dict);
 		list<Posting> parseAndExecute(string query);
 		list<Posting> positionalParseAndExecute(string query);
+		list<Posting> fuzzyParseAndExecute(string query);
 };
 
 
